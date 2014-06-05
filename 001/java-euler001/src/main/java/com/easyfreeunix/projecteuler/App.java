36c72301ff94d7358d04f1d0d9ae0fd99ee0b905
Tim Hawes <tim@easyfreeunix.com>
2014-06-05 10:42:40 -0400

    Added java solution to problem 1

---
+++ 001/java-euler001/src/main/java/com/easyfreeunix/projecteuler/App.java
@@ -0,0 +1,18 @@
+package com.easyfreeunix.projecteuler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int sum = 0;
+        for (int i = 0; i < 1000;i++) {
+              if ((i % 3 == 0) || (i % 5 == 0))
+                        sum = sum + i;
+        }
+        System.out.println(String.valueOf(sum));
+    }
+}
