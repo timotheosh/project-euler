43338dac0769d054ea1143c34e4be46297d2258e
Tim Hawes <tim@easyfreeunix.com>
2014-06-06 07:02:43 -0400

    Started on problem 3

---
+++ 003/clojure-euler003/test/clojure_euler003/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-euler003.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-euler003.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
---
+++ 003/python/euler003.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2.7
+
+from math import sqrt
+
+def primeFactors(n):
+    pf = []
+
+    # Add the number of 2's that divide n
+    while n % 2 == 0:
+        pf.append(n)
+        n = n/2
+
+    # n must be odd at this point, So we can skip one element.
+    i = 3
+    while i <= sqrt(n):
+        while (n%i == 0):
+            pf.append(i)
+            n = n/i
+        i = i + 2
+
+    # This condition is to handle the case when n is a prime number
+    # greater than 2
+    if n > 2:
+        pf.append(n)
+
+    return pf
+
+l = primeFactors(600851475143)
+print  l
